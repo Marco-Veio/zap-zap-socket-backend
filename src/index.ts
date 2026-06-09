ba843bddcfad604cb584e744171c6dabbc317622
Marco Véio <marcoarthomaz@gmail.com>
2026-06-09 11:49:44 -0300

    adding images

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT);
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { upload } from "../config/multer";
 
 import messageController from "./controllers/message";
+import imageController from "./controllers/image";
 
 const router = Router();
 
@@ -8,5 +10,8 @@ router.get("/", (request, response) => response.json("Hello World!"));
 
 // Rotas de mensagens
 router.post("/messages", messageController.create);
+
+// Rotas de imagens
+router.post("/images", upload.single("image"), imageController.create);
 
 export default router;
